d3e6ccface055a92aa7e2f44b0d2b7b7d2457989
Jared Patterson <jpwesley34@gmail.com>
2024-05-02 10:47:51 -0700

    Passes tests.

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,20 @@
 def badge_maker(name):
-    return None
+    return f'Hello, my name is {name}.'
 
 def batch_badge_creator(names):
-    return None
+    batch = []
+    for name in names:
+        batch.append(f'Hello, my name is {name}.')
+    return batch
 
 def assign_rooms(names):
-    return None
+    room_assignment = []
+    for n in range(8):
+        room_assignment.append(f"Hello, {names[n]}! You'll be assigned to room {n+1}!")
+    return room_assignment
 
 def printer(names):
-    return None
+    for badge in batch_badge_creator(names):
+        print(badge)
+    for room in assign_rooms(names):
+        print(room)
